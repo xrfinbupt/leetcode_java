d70218f0e3b803196b6168e761dee8332e95dc35
荣飞 <xurongfei@stonewise.cn>
2021-01-05 09:49:41 +0800

    每日磨刀

---
+++ src/main/java/high_frequency/easy/no11_merge_sorted_array.java
@@ -0,0 +1,43 @@
+package high_frequency.easy;
+
+/**
+ * https://github.com/azl397985856/leetcode/blob/master/problems/88.merge-sorted-array.md
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ * <p>
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ * <p>
+ * 说明:
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * 示例:
+ * <p>
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * <p>
+ * 输出: [1,2,2,3,5,6]
+ */
+public class no11_merge_sorted_array {
+    /**
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || nums1.length < 1 || nums2 == null || nums2.length < 1 || n < 1) return;
+
+        int i = m - 1;
+        int j = n - 1;
+        for (int k = m + n - 1; k >= 0; k--) {
+            if (j == -1) return;
+            if (i == -1 || nums2[j] >= nums1[i]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+        }
+    }
+}
