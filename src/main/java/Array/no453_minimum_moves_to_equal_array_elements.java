375d2413dbd9e5137af249807afd4d5ef20c12ab
荣飞 <xurongfei@stonewise.cn>
2021-01-31 10:52:40 +0800

    每日磨刀 #35题

--- src/main/java/Array/no453_minimum_moves_to_equal_array_elements.java
+++ src/main/java/Array/no453_minimum_moves_to_equal_array_elements.java
@@ -19,14 +19,14 @@ import java.util.Collections;
  */
 public class no453_minimum_moves_to_equal_array_elements {
     /**
-     * 我这种方案超时了
+     * 方法1 我自己想的这种方案超时了 下面几种方法是参考官方答案写的
      * @param nums
      * @return
      */
-    public int minMoves(int[] nums) {
-        if(nums==null || nums.length < 2) return 0;
-        if(nums.length==2){
-            return Math.abs(nums[0]-nums[1]);
+    public int minMoves_method1(int[] nums) {
+        if (nums == null || nums.length < 2) return 0;
+        if (nums.length == 2) {
+            return Math.abs(nums[0] - nums[1]);
         }
         Arrays.sort(nums);
 
@@ -43,14 +43,76 @@ public class no453_minimum_moves_to_equal_array_elements {
                     if (nums[i] != nums[len - 1]) break;
                 }
                 if (i < 0) return count;
-                else{
-                    Arrays.sort(nums,0,len-1);
+                else {
+                    Arrays.sort(nums, 0, len - 1);
                 }
 
                 count++;
                 for (i = 0; i < len - 1; i++) nums[i]++;
             }
         }
+    }
+
+    /**
+     * 方法2：排序后
+     * @param nums
+     * @return
+     */
+    public int minMoves_method2(int[] nums) {
+        if (nums == null || nums.length < 2) return 0;
+        if (nums.length == 2) {
+            return Math.abs(nums[0] - nums[1]);
+        }
+        Arrays.sort(nums);
+
+        int len = nums.length;
+        int moveCount = 0;
+        for (int i = 1; i < len; i++) {
+            moveCount += (nums[i] - nums[0]);
+        }
+        return moveCount;
+    }
+    /**
+     * 方法3：不用排序 选择一个最小值即可 就是数学法
+     * @param nums
+     * @return
+     */
+    public int minMoves(int[] nums) {
+        if (nums == null || nums.length < 2) return 0;
+        if (nums.length == 2) {
+            return Math.abs(nums[0] - nums[1]);
+        }
+        int min = Integer.MAX_VALUE;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            min = Math.min(nums[i], min);
+        }
+        int moveCount = 0;
+        for (int i = 0; i < len; i++) {
+            moveCount += (nums[i] - min);
+        }
+        return moveCount;
+    }
+    /**
+     * 方法4：动态规划 需要先排序
+     * @param nums
+     * @return
+     */
+    public int minMoves_method4(int[] nums) {
+        if (nums == null || nums.length < 2) return 0;
+        if (nums.length == 2) {
+            return Math.abs(nums[0] - nums[1]);
+        }
+        Arrays.sort(nums);
+
+        int len = nums.length;
+        int moveCount = 0;
+        for (int i = 1; i < len; i++) {
+            int diff = moveCount + nums[i] - nums[i-1];
+            nums[i] += moveCount;
+            moveCount += diff;
+        }
+        return moveCount;
     }
     public static void main(String args[]){
         no453_minimum_moves_to_equal_array_elements obj = new no453_minimum_moves_to_equal_array_elements();
