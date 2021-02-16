38a3b4c0f93fcf2be9caa74c56fbc414fd20d3c5
荣飞 <xurongfei@stonewise.cn>
2021-02-16 08:05:34 +0800

    每日磨刀 #39题

--- src/main/java/Array/no16_3sum_closest.java
+++ src/main/java/Array/no16_3sum_closest.java
@@ -59,20 +59,68 @@ public class no16_3sum_closest {
         return result;
     }
 
+    /**
+     * 排序+双指针 (优化)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumCloses2(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left != right){
+                int min = nums[i] + nums[left] + nums[left + 1];
+                if(target < min){
+                    if(Math.abs(result - target) > Math.abs(min - target))
+                        result = min;
+                    break;
+                }
+                int max = nums[i] + nums[right] + nums[right - 1];
+                if(target > max){
+                    if(Math.abs(result - target) > Math.abs(max - target))
+                        result = max;
+                    break;
+                }
+                int sum = nums[i] + nums[left] + nums[right];
+                // 判断三数之和是否等于target
+                if(sum == target)
+                    return sum;
+                if(Math.abs(sum - target) < Math.abs(result - target))
+                    result = sum;
+                if(sum > target){
+                    right--;
+                    while(left != right && nums[right] == nums[right+1])
+                        right--;
+                }
+                else{
+                    left++;
+                    while(left != right && nums[left] == nums[left-1])
+                        left++;
+                }
+            }
+            while(i<nums.length-2 && nums[i] == nums[i+1])
+                i++;
+        }
+        return result;
+    }
+
     public static void main(String args[]) {
         no16_3sum_closest obj = new no16_3sum_closest();
         int[] nums = new int[]{-1,2,1,-4};
-        int result = obj.threeSumClosest1(nums,1);
+        int result = obj.threeSumCloses2(nums,1);
         System.out.println("2:"+result);
         System.out.println("---------");
 
         nums = new int[]{4,0,5,-5,3,3,0,-4,-5};
-        result = obj.threeSumClosest1(nums,-2);
+        result = obj.threeSumCloses2(nums,-2);
         System.out.println("-2:"+result);
         System.out.println("---------");
 
         nums = new int[]{-55,-24,-18,-11,-7,-3,4,5,6,9,11,23,33};
-        result = obj.threeSumClosest1(nums,0);
+        result = obj.threeSumCloses2(nums,0);
         System.out.println("0:"+result);
         System.out.println("--------");
     }
