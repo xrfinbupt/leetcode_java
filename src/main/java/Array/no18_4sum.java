c3d8f5ce1412b770131342de9d9710abd3dea055
荣飞 <xurongfei@stonewise.cn>
2021-02-17 09:29:33 +0800

    每日磨刀 #40题

---
+++ src/main/java/Array/no18_4sum.java
@@ -0,0 +1,96 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+ * 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ * 注意：
+ * 答案中不可以包含重复的四元组。
+ *
+ * 示例：
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ *
+ * 满足要求的四元组集合为：
+ * [
+ *   [-1,  0, 0, 1],
+ *   [-2, -1, 1, 2],
+ *   [-2,  0, 0, 2]
+ * ]
+ *
+ * https://leetcode-cn.com/problems/4sum
+ */
+public class no18_4sum {
+
+    /**
+     * 遇到一个问题：两次提交代码一样 执行时间不一样 可能后台服务器负载不同 影响了结果
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) return result;
+        Arrays.sort(nums);
+
+        int len = nums.length;
+        if(nums[0]+nums[1]+nums[2]+nums[3] > target) return result;
+        if(nums[len-4]+nums[len-3]+nums[len-2]+nums[len-1]<target) return result;
+
+        for (int i = 0; i < len-3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if(nums[i] + nums[i+1] + nums[i+2] + nums[i+3]>target) break;
+            if(nums[i] + nums[len-3] + nums[len-2] + nums[len-1]<target) continue;
+
+
+            for (int ii = i+1; ii < len-2; ii++) {
+                if (ii > i+1 && nums[ii] == nums[ii - 1]) continue;
+                if(nums[i] + nums[ii] + nums[ii+1] + nums[ii+2]>target) break;
+                if(nums[i] + nums[ii] + nums[len-2] + nums[len-1]<target) continue;
+
+
+                int j = ii + 1;
+                int k = len - 1;
+                while (j < k) {
+                    int val = nums[i] + nums[ii] + nums[j] + nums[k];
+                    if (val == target) {
+                        List<Integer> tempResult = new ArrayList<>();
+                        tempResult.add(nums[i]);
+                        tempResult.add(nums[ii]);
+                        tempResult.add(nums[j]);
+                        tempResult.add(nums[k]);
+                        result.add(tempResult);
+                        while (j + 1 <= len - 1 && nums[j] == nums[j + 1]) {
+                            j++;
+                        }
+                        while (k - 1 > j && nums[k] == nums[k - 1]) {
+                            k--;
+                        }
+                        j++;
+                        k--;
+                    } else if (val - target > 0) {
+                        if(j+1 <= k && nums[i] + nums[ii] + nums[j] + nums[j+1]>target) break;
+
+                        k--;
+                    } else if (val-target < 0) {
+                        if(j <= k-1 && nums[i] + nums[ii] + nums[k-1] + nums[k]<target) break;
+
+                        j++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    public static void main(String args[]){
+        no18_4sum obj = new no18_4sum();
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+        List<List<Integer>> result = obj.fourSum(nums,0);
+        System.out.println(result);
+        System.out.println("---------");
+    }
+}
