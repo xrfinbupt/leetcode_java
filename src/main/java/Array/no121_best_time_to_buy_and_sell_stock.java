b4215207663b3a14b3cdf328904c9633d3134938
荣飞 <xurongfei@stonewise.cn>
2021-03-18 20:19:35 +0800

    每日磨刀 温习array

--- src/main/java/Array/no121_best_time_to_buy_and_sell_stock.java
+++ src/main/java/Array/no121_best_time_to_buy_and_sell_stock.java
@@ -1,6 +1,7 @@
 package Array;
 
 /**
+ * 121. 买卖股票的最佳时机
  * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
  * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
  * 注意：你不能在买入股票前卖出股票。
@@ -14,6 +15,10 @@ package Array;
  * 输入: [7,6,4,3,1]
  * 输出: 0
  * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 提示：
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
  *
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/description/
  * https://github.com/azl397985856/leetcode/blob/master/problems/121.best-time-to-buy-and-sell-stock.md
