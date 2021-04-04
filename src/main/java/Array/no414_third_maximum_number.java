e76c92454de132f31ab3392762e808993625440f
荣飞 <xurongfei@stonewise.cn>
2021-04-04 08:42:50 +0800

    每日磨刀 温习array

--- src/main/java/Array/no414_third_maximum_number.java
+++ src/main/java/Array/no414_third_maximum_number.java
@@ -3,6 +3,8 @@ package Array;
 import java.util.Arrays;
 
 /**
+ * 414. 第三大的数
+ *
  * 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
  *
  * 示例 1：
@@ -18,10 +20,14 @@ import java.util.Arrays;
  * 示例 3：
  * 输入：[2, 2, 3, 1]
  * 输出：1
- *
  * 解释：注意，要求返回第三大的数，是指第三大且唯一出现的数。
  * 存在两个值为2的数，它们都排第二。
  *
+ * 提示：
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *  
+ * 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
  * https://leetcode-cn.com/problems/third-maximum-number
  *
  */
