b26952179737ccc5d82a29f1171a1329902fafbb
荣飞 <xurongfei@stonewise.cn>
2021-04-16 22:49:19 +0800

    每日磨刀 #65题

---
+++ src/main/java/Array/no34_find_first_and_last_position_of_element_in_sorted_array.java
@@ -0,0 +1,94 @@
+package Array;
+
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 进阶：
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ *  
+ *
+ * 示例 1：
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ *
+ * 示例 2：
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ *
+ * 示例 3：
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *  
+ *
+ * 提示：
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums 是一个非递减数组
+ * -10^9 <= target <= 10^9
+ *
+ * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+ *
+ */
+public class no34_find_first_and_last_position_of_element_in_sorted_array {
+    /**
+     * 二分法 参考:https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/da-jia-bu-yao-kan-labuladong-de-jie-fa-fei-chang-2/
+     *
+     * 形式:
+     * 1 while (left <= right) 简单问题用，在循环体里能找到答案以后退出
+     * 2 while (left < right)  复杂问题用，把答案留到退出循环以后，再判断
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int result[] = new int[]{-1, -1};
+        if (nums == null || nums.length == 0) return result;
+
+        int first = findFirstPos(nums, target);
+        if (first < 0) {
+            return result;
+        }
+
+        int last = findLastPos(nums, target);
+        return new int[]{first, last};
+    }
+
+    public int findFirstPos(int nums[], int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if (left < len && nums[left] == target) return left;
+        return -1;
+    }
+
+    public int findLastPos(int nums[], int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+}
