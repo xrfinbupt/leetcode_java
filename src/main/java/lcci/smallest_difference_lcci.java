62a71aae9b3a466262787ec1eddc5869e467e809
荣飞 <xurongfei@stonewise.cn>
2021-05-01 08:50:11 +0800

    每日磨刀 #73题

---
+++ src/main/java/lcci/smallest_difference_lcci.java
@@ -0,0 +1,66 @@
+package lcci;
+
+import java.util.Arrays;
+
+/**
+ * 面试题 16.06. 最小差
+ *
+ * 给定两个整数数组a和b，计算具有最小差绝对值的一对数值（每个数组中取一个值），并返回该对数值的差
+ *
+ * 示例：
+ * 输入：{1, 3, 15, 11, 2}, {23, 127, 235, 19, 8}
+ * 输出：3，即数值对(11, 8)
+ *  
+ *
+ * 提示：
+ * 1 <= a.length, b.length <= 100000
+ * -2147483648 <= a[i], b[i] <= 2147483647
+ * 正确结果在区间 [0, 2147483647] 内
+ *
+ * 链接：https://leetcode-cn.com/problems/smallest-difference-lcci
+ *
+ */
+public class smallest_difference_lcci {
+    /**
+     * 这种方法时间复杂度较高
+     * @param a
+     * @param b
+     * @return
+     */
+    public int smallestDifference(int[] a, int[] b) {
+        int[] shortArray = a;
+        int[] longArray = b;
+        if (a.length > b.length) {
+            shortArray = b;
+            longArray = a;
+        }
+        long minDiff = Integer.MAX_VALUE;
+        Arrays.sort(longArray);
+        for (int i = 0; i < shortArray.length; i++) {
+            long diff = binarySearchDiff(longArray, shortArray[i]);
+            if (minDiff > diff) minDiff = diff;
+        }
+        return (int) minDiff;
+    }
+
+    // abs.(diff)
+    public long binarySearchDiff(int[] array, int target) {
+        int len = array.length;
+        int l = 0, r = len - 1;
+        long minDiff = Integer.MAX_VALUE;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long diff = array[mid] - target;
+
+            if (diff == 0) {
+                return 0;
+            } else if (diff > 0) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+            if (minDiff > Math.abs(diff)) minDiff = Math.abs(diff);
+        }
+        return minDiff;
+    }
+}
