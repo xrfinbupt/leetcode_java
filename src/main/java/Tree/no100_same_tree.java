6b34483cf3256441172a8933abf217c61c69c510
荣飞 <xurongfei@stonewise.cn>
2021-05-23 21:56:00 +0800

    每日磨刀 #113题

---
+++ src/main/java/Tree/no100_same_tree.java
@@ -0,0 +1,39 @@
+package Tree;
+
+import common.TreeNode;
+
+/**
+ * 100. 相同的树
+ *
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1：
+ * 输入：p = [1,2,3], q = [1,2,3]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：p = [1,2], q = [1,null,2]
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：p = [1,2,1], q = [1,1,2]
+ * 输出：false
+ *  
+ * 提示：
+ * 两棵树上的节点数目都在范围 [0, 100] 内
+ * -10^4 <= Node.val <= 10^4
+ *
+ * 链接：https://leetcode-cn.com/problems/same-tree
+ */
+public class no100_same_tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
