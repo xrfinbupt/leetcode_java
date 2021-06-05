11ce24fcd66e9eb9cbf8f825ab05143bd02d215f
荣飞 <xurongfei@stonewise.cn>
2021-06-05 15:53:08 +0800

    每日磨刀 #125题

--- src/main/java/String/no10_regular_expression_matching.java
+++ src/main/java/String/no10_regular_expression_matching.java
@@ -43,10 +43,89 @@ package String;
  * 链接：https://leetcode-cn.com/problems/regular-expression-matching
  */
 public class no10_regular_expression_matching {
-    // 动态规划 回溯
+    boolean result = false;
+
+    /**
+     * 执行用时：65 ms, 在所有 Java 提交中击败了10.39%的用户
+     * 内存消耗：37 MB, 在所有 Java 提交中击败了92.45%的用户
+     *
+     * @param s
+     * @param p
+     * @return
+     */
     public boolean isMatch(String s, String p) {
+        result = false;
         // * 前面有字符 和 .
         // . 不需要关心前面字符
+        int sLen = s.length();
+        int pLen = p.length();
+        if(sLen == pLen && sLen == 0) return true;
+
+        sub_isMatch(s, p, sLen, pLen, 0, 0, -1);
+        if (result) return result;
+
         return false;
+    }
+    // p=ab*  ab.        ab*c
+    // s=a ab abb abbb   ac
+    public void sub_isMatch(String s, String p, int sLen, int pLen, int sPos, int pPos, int width) {
+        if(pPos >= pLen && sPos >=sLen ) result = true;
+        if(pPos >= pLen || result) return;
+
+        char laterCh = 0;
+        if (pPos < pLen - 1) laterCh = p.charAt(pPos + 1);
+        char pCurr = p.charAt(pPos);
+        if (laterCh == '*') {
+            if (width == -1) {
+                int w = 0;
+                while (sPos + w <= sLen) {
+                    if (result) return;
+
+                    sub_isMatch(s, p, sLen, pLen, sPos, pPos, w);
+                    w++;
+                }
+            } else if (width == 0) {
+                sub_isMatch(s, p, sLen, pLen, sPos, pPos + 2, -1);
+            } else {
+                // width >= 1
+                int w = 0;
+                while (w < width) {
+                    char ch2 = s.charAt(sPos + w);
+                    if (ch2 == pCurr || pCurr == '.') w++;
+                    else {
+                        break;
+                    }
+                }
+                if (w == width) sub_isMatch(s, p, sLen, pLen, sPos + width, pPos + 2, -1);
+            }
+        } else {
+            char sCurr = 0;
+            if(sPos<sLen) sCurr = s.charAt(sPos);
+
+            if (pCurr == '.'&& sCurr!=0 || sCurr == pCurr) {
+                sub_isMatch(s, p, sLen, pLen, sPos + 1, pPos + 1, -1);
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        no10_regular_expression_matching obj = new no10_regular_expression_matching();
+        System.out.println("\"\",\"\"="+obj.isMatch("",""));
+        System.out.println("\"\",\".\"="+obj.isMatch("","."));
+        System.out.println("\"a\",\"a.\"="+obj.isMatch("a","a."));
+        System.out.println("\"a\",\"ab*\"="+obj.isMatch("a","ab*"));
+        System.out.println("\"ab\",\"ab*\"="+obj.isMatch("ab","ab*"));
+        System.out.println("\"abb\",\"ab*\"="+obj.isMatch("abb","ab*"));
+        System.out.println("\"abbb\",\"ab*\"="+obj.isMatch("abbb","ab*"));
+        System.out.println("\"abb\",\"ab.\"="+obj.isMatch("abb","ab."));
+        System.out.println("\"abc\",\"ab*c\"="+obj.isMatch("abc","ab*c"));
+        System.out.println("\"ac\",\"ab*c\"="+obj.isMatch("ac","ab*c"));
+
+        System.out.println("\"aa\",\"a\"="+obj.isMatch("aa","a"));
+        System.out.println("\"aa\",\"a*\"="+obj.isMatch("aa","a*"));
+        System.out.println("\"ab\",\".*\"="+obj.isMatch("ab",".*"));
+        System.out.println("\"aab\",\"c*a*b\"="+obj.isMatch("aab","c*a*b"));
+        System.out.println("\"mississippi\",\"mis*is*p*.\"="+obj.isMatch("mississippi","mis*is*p*."));
+        System.out.println("\"ab\",\"..\"="+obj.isMatch("ab",".."));
     }
 }
