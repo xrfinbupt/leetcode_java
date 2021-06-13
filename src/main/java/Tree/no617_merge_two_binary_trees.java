fd3a1efc668d875fa3f56f5f9c6d554776d99c6c
荣飞 <xurongfei@stonewise.cn>
2021-06-14 07:43:58 +0800

    每日磨刀 #156题

---
+++ src/main/java/Tree/no617_merge_two_binary_trees.java
@@ -0,0 +1,45 @@
+package Tree;
+
+import common.TreeNode;
+
+/**
+ * 617. 合并二叉树
+ *
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ *
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 示例 1:
+ * 输入:
+ * 	Tree 1                     Tree 2
+ *           1                         2
+ *          / \                       / \
+ *         3   2                     1   3
+ *        /                           \   \
+ *       5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 	     3
+ * 	    / \
+ * 	   4   5
+ * 	  / \   \
+ * 	 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ *
+ * 链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+ */
+public class no617_merge_two_binary_trees {
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.7 MB, 在所有 Java 提交中击败了61.56%的用户
+     */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+        root1.val = root1.val + root2.val;
+
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+        return root1;
+    }
+}
