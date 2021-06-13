4faffd95094f0035cd0e7f2e7d39588ccac2f55b
荣飞 <xurongfei@stonewise.cn>
2021-06-13 14:02:59 +0800

    每日磨刀 #152题

---
+++ src/main/java/lcof/no6_reversePrint.java
@@ -0,0 +1,27 @@
+package lcof;
+
+import common.ListNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class no6_reversePrint {
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了38.16%的用户
+     * 内存消耗：38.8 MB, 在所有 Java 提交中击败了90.13%的用户
+     */
+    public int[] reversePrint(ListNode head) {
+        Deque<Integer> result = new ArrayDeque<>();
+        while(head!=null){
+            result.addFirst(head.val);
+            head = head.next;
+        }
+        int array[] = new int[result.size()];
+        int i = 0;
+        while(result.size()>0) array[i++] = result.removeFirst();
+
+        return array;
+    }
+}
